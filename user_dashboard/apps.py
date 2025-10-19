c087436da0224b8127b7ff01ffca7467e560e350
zoh-f <zohfais@gmail.com>
2025-10-19 01:28:56 -0400

    Admin capabilities for admin email

--- user_dashboard/apps.py
+++ user_dashboard/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 class UserDashboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user_dashboard'
+
+def ready(self):
+    import user_dashboard.signals

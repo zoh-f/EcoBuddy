1759d3e3a132ded5a9665296f149ce6b152b26cf
wangrui04 <wangruielena@gmail.com>
2025-10-27 20:20:03 -0400

    added post stuff

---
+++ user_dashboard/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'image']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter title'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter content'}),
+        }

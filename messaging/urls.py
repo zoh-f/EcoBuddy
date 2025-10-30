bb018cc5ee6d4ae99ac9a23a60592d83a14b88fc
zoh-f <zohfais@gmail.com>
2025-10-30 14:32:56 -0400

    Messaging fixes, auto-update

--- messaging/urls.py
+++ messaging/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('lobby/', views.lobby, name='lobby'),
     path('chat/<str:recipient_username>/', views.chat, name='chat'),
     path('create-message/', views.create_message, name='create-message'),
-    path('stream-chat-messages/', views.stream_chat_messages, name='stream-chat-messages'),
+    path("stream-chat-messages/<str:recipient_username>/", views.stream_chat_messages, name='stream-chat-messages'),
 ]

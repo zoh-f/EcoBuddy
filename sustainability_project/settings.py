ad66f873e897a962bd8ad0c9560e6ec854a4c722
Kabir Minhas <princekm.cool@gmail.com>
2025-12-09 12:23:11 -0500

    Fixed signin and signout UI

--- sustainability_project/settings.py
+++ sustainability_project/settings.py
@@ -102,7 +102,7 @@ ROOT_URLCONF = 'sustainability_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
